731eabd93ddd51b723cde5e86bc1ae9bbe6daad9
swirly-way <s4m14z1l@gmail.com>
2024-12-02 18:35:27 +0000

    Add final function that solves the third MVP

--- ticket2.js
+++ ticket2.js
@@ -1,9 +1,28 @@
 
 function displayTemperatureInCelsius(fahrenheit){
-return (fahrenheit - 32) /1.8;
-}
-console.log(displayTemperatureInCelsius(10));
+    return (fahrenheit - 32) /1.8;
+    }
 
+function displayTemperatureInFahrenheit(celsius){
+    return celsius * 1.8 + 32;
+    }
+
+
+    let defaultMeasure = 'celsius'; //This variable stores the new default, either celsius or fahrenheit.
+    let newTemperature = 0; //This variable stores the new temperature after it has been converted.
+
+    function toggleTemperatureDisplay(temperature){
+    
+    if(defaultMeasure === 'celsius'){
+        defaultMeasure = 'fahrenheit';
+        newTemperature = displayTemperatureInFahrenheit(temperature);
+    }
+    else if(defaultMeasure === 'fahrenheit') {
+     defaultMeasure = 'celsius',
+     newTemperature = displayTemperatureInCelsius(temperature);
+    }
+     return newTemperature;
+}
 
 
 /*
@@ -16,7 +35,18 @@ Develop a JavaScript function displayTemperatureInCelsius
 
 MVP 2
 Implement another function displayTemperatureInFahrenheit that performs the inverse operation.
+// Copy function
+// Change name
+// Change parameter names
+// update conversion formula
 
 MVP 3
-Create a function toggleTemperatureDisplay that returns either Celsius or Fahrenheit depending on which was the last returned (if the last value returned was Fahrenheit, then you’ll want to return Celsius and vice versa). Start with a default of the last returned format being Celsius.
+//Create a function toggleTemperatureDisplay
+ that returns either Celsius or Fahrenheit depending on which was the last returned 
+ //(if the last value returned was Fahrenheit, then you’ll want to return Celsius and vice versa).
+  Start with a default of the last returned format being Celsius.
+
+ // Save the default measure in a variable as a string that will toggle between 'celsius' and 'fahrenheit'
+ // Set an if conditional that will choose which operation to perform based on the default measure string.
+ // Toggle default variable value and return result.
 */
