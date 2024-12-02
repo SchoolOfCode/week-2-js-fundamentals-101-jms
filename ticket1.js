76946a3476149af3ecb2a09bdd7984ea19adf62f
swirly-way <s4m14z1l@gmail.com>
2024-12-02 17:17:05 +0000

    Clean around the code to make MVP comments more presentable

--- ticket1.js
+++ ticket1.js
@@ -18,28 +18,6 @@ function convertGBPToCurrency(code, amount){
    }
 }
 
-//MVP 3
-//Write a function convertGBPToCurrency 
-//which takes in both a currency code ("EUR" or "USD") and an amount of £ to convert.
-// It should, based on which code has been entered, use one of your previously written functions to return the correct conversion. 
-//If an unknown code is entered, it should return null.
-
-
-
-
-
-
-/*
-convertGBPToCurrency(amount, currency);
-let amount = 40;
-let currency = '$';
-console.log(integer ); /*
-
-//let userInput = '40$' ---> A40 + B'$'  
-
-//£10 OR 10$
-
-
 //MVP 1
 //Write a JavaScript function convertGBPtoEUR that takes in a specified amount in £
 //and converts it to € using a given exchange rate. 
@@ -51,11 +29,10 @@ console.log(integer ); /*
 //and converts it to $ using a given exchange rate.
 //It should behave in a very similar way to the previous function.
 
+//MVP 3
+//Write a function convertGBPToCurrency 
+//which takes in both a currency code ("EUR" or "USD") and an amount of £ to convert.
+// It should, based on which code has been entered, use one of your previously written functions to return the correct conversion. 
+//If an unknown code is entered, it should return null.
 
 
-/* function(currency){
-first step is differentiate between currency
-if string contains '$' then --->
-
-}
-*/
